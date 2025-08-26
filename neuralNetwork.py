fedd1fa43d6b62775bdd231de9b2b418d68b417f
Yuanqing <yuanqing.wang@qq.com>
2025-08-26 09:54:33 -0500

    Add files via upload

---
+++ neuralNetwork.py
@@ -0,0 +1,111 @@
+import numpy
+import scipy.special
+import matplotlib.pyplot
+
+
+class neuralNetwork:
+
+    def __init__(self, inputnodes, hiddennodes, outputnodes, learningrate):
+        self.inodes = inputnodes
+        self.hnodes = hiddennodes
+        self.onodes = outputnodes
+
+        self.wih = (numpy.random.normal(0.0, pow(self.hnodes, -0.5), (self.hnodes, self.inodes)))
+
+        self.who = (numpy.random.normal(0.0, pow(self.onodes, -0.5), (self.onodes, self.hnodes)))
+
+
+        self.activation_function = lambda x: scipy.special.expit(x)
+
+        self.lr = learningrate
+
+    def train(self, inputs_list, targets_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+
+        final_outputs = self.activation_function(final_inputs)
+
+        output_errors = targets - final_outputs
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        self.who += self.lr * numpy.dot((output_errors *final_outputs * (1-final_outputs)), numpy.transpose(hidden_outputs))
+
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1-hidden_outputs)), numpy.transpose(inputs))
+
+        pass
+
+    def query(self, inputs_list):
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        hidden_outputs = self.activation_function(hidden_inputs)
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        final_outputs = self.activation_function(final_inputs)
+        return final_outputs
+
+input_nodes = 784
+hidden_nodes = 100
+output_nodes = 10
+
+learning_rate = 0.3
+
+n = neuralNetwork(input_nodes,hidden_nodes,output_nodes, learning_rate)
+
+training_data_file = open("D:/Programming/pycharm unified/data/mnist_train_100.csv", 'r')
+training_data_list = training_data_file.readlines()
+training_data_file.close()
+
+epochs = 5
+
+for e in range(epochs):
+# go through all records in the training data set
+    for record in training_data_list:
+# split the record by the ',' commas
+        all_values = record.split(',')
+# scale and shift the inputs
+        inputs = (numpy.asarray(all_values[1:]).astype(float) / 255.0 * 0.99) + 0.01
+
+# create the target output values (all 0.01, except the desired label which is 0.99)
+        targets = numpy.zeros(output_nodes) + 0.01
+# all_values[0] is the target label for this record
+        targets[int(all_values[0])] = 0.99
+        n.train(inputs, targets)
+    pass
+pass
+
+
+test_data_file = open("D:/Programming/pycharm unified/data/mnist_test_10.csv", 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
+
+scorecard = []
+# go through all the records in the test data set
+for record in test_data_list:
+# split the record by the ',' commas
+    all_values = record.split(',')
+# correct answer is first value
+    correct_label = int(all_values[0])
+# scale and shift the inputs
+    inputs = (numpy.asarray(all_values[1:]).astype(float) / 255.0 * 0.99) + 0.01
+# query the network
+    outputs = n.query(inputs)
+# the index of the highest value corresponds to the label
+    label = numpy.argmax(outputs)
+# append correct or incorrect to list
+    if (label == correct_label):
+# network's answer matches correct answer, add 1 toscorecard
+        scorecard.append(1)
+    else:
+# network's answer doesn't match correct answer, add 0 to scorecard
+        scorecard.append(0)
+pass
+
+
+scorecard_array = numpy.asarray(scorecard)
+print ("performance = ", scorecard_array.sum() /
+scorecard_array.size)
+
